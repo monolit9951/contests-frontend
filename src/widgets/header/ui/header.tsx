3eaf1bf2ea02e9fcd40fbbcede1d2f90d1b72267
Ura427 <urakost427@gmail.com>
2024-04-29 13:54:13 +0300

    Made header desing according to figma

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,17 +1,17 @@
 import { Input } from 'shared/ui/input'
 
 import './header.scss'
+import { Searchbar } from 'shared/ui/Searchbar'
+import { UserPanel } from 'shared/ui/UserPanel/ui/userPanel'
+import { Logo } from 'shared/ui/logo/ui/logo'
 
 export const Header = () => {
 
     return (
         <nav className="header">
-            <h1>Logotype</h1>
-            <div className="input-box">
-                <Input
-                    placeholder='Button books by title, author, ISBN or keywords'
-                />
-            </div>
+            <Logo/>
+            <Searchbar/>
+            <UserPanel/>
         </nav>
     )
 }
