77ac26ae514f1823f1d25af75a26fdb461d9e08f
Neptune-Dmitry <dmitry.popko@neptunessushi.com>
2024-04-29 16:48:36 +0300

    feat: add Textarea and edit Input

--- src/shared/ui/input/index.ts
+++ src/shared/ui/input/index.ts
@@ -1,1 +1,6 @@
-export { Input } from './input'
+import Checkbox from './Checkbox/Checkbox'
+import Radio from './Radio/Radio'
+import Textarea from './Textarea/Textarea'
+import Input from './Input'
+
+export { Checkbox, Input, Radio, Textarea }
