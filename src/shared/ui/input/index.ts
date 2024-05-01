8119bcc904a58a0addc21b6a8e0d555cd50cac55
Neptune-Dmitry <dmitry.popko@neptunessushi.com>
2024-05-01 13:55:59 +0300

    fix: reset shared-ui / delete

--- src/shared/ui/input/index.ts
+++
@@ -1,6 +0,0 @@
-import Checkbox from './Checkbox/Checkbox'
-import Radio from './Radio/Radio'
-import Textarea from './Textarea/Textarea'
-import Input from './input'
-
-export { Checkbox, Input, Radio, Textarea }
