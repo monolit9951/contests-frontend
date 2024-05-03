c4e983d961310440fc37f2ba7d9dee080de86f13
Ura427 <urakost427@gmail.com>
2024-05-03 11:00:27 +0300

    Add state for filter

--- src/shared/ui/contestsFilter/ui/contestsFilter.tsx
+++ src/shared/ui/contestsFilter/ui/contestsFilter.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
+
 import "./contestsFilter.scss"
 
+
+enum FilterOptions {
+    All,
+    For_fun,
+    For_work
+}
+
 export const ContestsFilter = () => {
+    const [currFilter, setCurrFilter] =useState<FilterOptions>(FilterOptions.All)
+
+    
     return (
         <div className="contestsFilter_container">
-            <button type="button" className="filter_option">All</button>
-            <button type="button" className="filter_option">For fun</button>
-            <button type="button" className="filter_option">For work</button>
+            <button type="button" className="filter_option" onClick={() => setCurrFilter(FilterOptions.All)}>All</button>
+            <button type="button" className="filter_option" onClick={() => setCurrFilter(FilterOptions.For_fun)}>For fun</button>
+            <button type="button" className="filter_option" onClick={() => setCurrFilter(FilterOptions.For_work)}>For work</button>
         </div>
     )
 }
