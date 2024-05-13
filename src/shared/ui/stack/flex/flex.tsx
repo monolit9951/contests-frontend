681e3deddcae4e333f2f969fcefa33b01a2888e6
VolodymyrKholiavko <166817393+VolodymyrKholiavko@users.noreply.github.com>
2024-05-13 16:30:41 +0300

    add: additional features

--- src/shared/ui/stack/flex/flex.tsx
+++ src/shared/ui/stack/flex/flex.tsx
@@ -1,41 +1,21 @@
-import { forwardRef, KeyboardEvent, MouseEvent, ReactNode } from 'react';
-import clsx from 'clsx';
+import { forwardRef, ReactNode } from 'react'
+import clsx from 'clsx'
 
-import './flex.scss';
+import './flex.scss'
 
 interface FlexProps {
-    children: ReactNode;
-    className?: string;
-    onClick?: (event: MouseEvent<HTMLDivElement>) => void;
+    children: ReactNode
+    className?: string
 }
 
 const Flex = forwardRef<HTMLDivElement, FlexProps>((props, ref) => {
-    const { children, className, onClick } = props;
-
-    const handleClick = (event: MouseEvent<HTMLDivElement>) => {
-        if (onClick) {
-            onClick(event);
-        }
-    };
-
-    const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === 'Enter' && onClick) {
-            onClick(event as unknown as MouseEvent<HTMLDivElement>);
-        }
-    };
+    const { children, className } = props
 
     return (
-        <div
-            ref={ref}
-            className={clsx('flex', className)}
-            onClick={handleClick}
-            onKeyDown={handleKeyDown}
-            tabIndex={0}
-            role="button"
-        >
+        <div ref={ref} className={clsx('flex', className)}>
             {children}
         </div>
-    );
-});
+    )
+})
 
-export default Flex;
+export default Flex
