c1b1cc06928b9a6f4b963d50659b350ead0b4162
Ura427 <urakost427@gmail.com>
2024-05-18 22:39:02 +0300

    KAN-3 create custom combobox component, modified input styles

---
+++ src/shared/ui/input/combobox/combobox.tsx
@@ -0,0 +1,81 @@
+import { ChangeEvent, SelectHTMLAttributes, useState } from 'react'
+import clsx from 'clsx'
+import alertIcon from 'shared/assets/icons/alert.svg?react'
+import infoIcon from 'shared/assets/icons/info.svg?react'
+import { Icon } from 'shared/ui/icon'
+import { HStack, VStack } from 'shared/ui/stack'
+import { Text } from 'shared/ui/text'
+
+import './combobox.scss'
+
+interface ICombobox extends SelectHTMLAttributes<HTMLSelectElement> {
+    label?: string
+    error?: string
+    info?: string
+    options: { value: string; label: string }[]
+    wrapperClassName?: string
+    className?: string
+    width?: string | number 
+}
+
+export default function Combobox(props: ICombobox) {
+    const {
+        name,
+        label,
+        error,
+        info,
+        value,
+        options,
+        placeholder,
+        onChange,
+        onBlur,
+        wrapperClassName,
+        className,
+        width, 
+        ...rest
+    } = props
+    const [selectedValue, setSelectedValue] = useState<string>('')
+
+    const onChangeSelectedValue = (e: ChangeEvent<HTMLSelectElement>) => {
+        setSelectedValue(e.target.value)
+    }
+
+    return (
+        <VStack className={clsx('combobox-wrapper', wrapperClassName)}>
+            {label && <label htmlFor={name}>{label}</label>}
+            <select
+                name={name}
+                value={value ?? selectedValue}
+                onChange={onChange ?? onChangeSelectedValue}
+                onBlur={onBlur}
+                className={clsx('combobox', error && 'error', className)}
+                style={{ width }} 
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...rest}
+            >
+                {placeholder && (
+                    <option value="" disabled className="placeholder-option">
+                        {placeholder}
+                    </option>
+                )}
+                {options.map((option) => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+            {info && (
+                <HStack className='combobox-wrapper__info'>
+                    <Icon Svg={infoIcon} />
+                    <Text Tag='span'>{info}</Text>
+                </HStack>
+            )}
+            {error && (
+                <HStack className='combobox-wrapper__error'>
+                    <Icon Svg={alertIcon} />
+                    <Text Tag='span'>{error}</Text>
+                </HStack>
+            )}
+        </VStack>
+    )
+}
--- src/shared/ui/input/index.ts
+++ src/shared/ui/input/index.ts
@@ -1,6 +1,7 @@
 import Checkbox from './checkbox/checkbox'
+import Combobox from './combobox/combobox'
 import Radio from './radio/radio'
 import Textarea from './textarea/textarea'
 import Input from './input'
 
-export { Checkbox, Input, Radio, Textarea }
+export { Checkbox, Combobox,Input, Radio, Textarea }
