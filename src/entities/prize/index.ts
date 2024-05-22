0d539f4d4c47fb3e911c83d6fa3c3b106f6b1e6a
Redddeve <popko.dmitry797@gmail.com>
2024-05-22 11:57:05 +0300

    feat: create Top Prize ui

--- src/entities/prize/index.ts
+++ src/entities/prize/index.ts
@@ -1,3 +1,6 @@
 import { Prize, PrizePreview, PrizeStructure, PrizeType } from './model/types'
+import TopPrize from './ui/topPrize'
 
 export type { Prize, PrizePreview, PrizeStructure, PrizeType }
+
+export { TopPrize }
