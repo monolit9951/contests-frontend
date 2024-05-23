0726534d62cec19b3f253bfa6a1fa76b0479f13b
VolodymyrKholiavko <166817393+VolodymyrKholiavko@users.noreply.github.com>
2024-05-23 12:28:30 +0300

    fix: deleting winners table from feed page

--- src/pages/feedPage/ui/feedPage.tsx
+++ src/pages/feedPage/ui/feedPage.tsx
@@ -1,5 +1,4 @@
 import VStack from 'shared/ui/stack/vStack/vStack'
-import {ContestWinnersTable} from "widgets/winnersTable";
 
 import "./feedPage.scss"
 
@@ -7,7 +6,6 @@ export const FeedPage = () => {
     return (
         <VStack className="feed-page-wrapper">
             <h1>FeedPage</h1>
-            <ContestWinnersTable/>
         </VStack>
     )
 }
