959860bdd3223d9379b8fd55d827141d803850f2
Ura427 <urakost427@gmail.com>
2024-05-29 11:45:47 +0300

    minor changes

--- src/shared/ui/prizePlace/ui/prizePlace.tsx
+++ src/shared/ui/prizePlace/ui/prizePlace.tsx
@@ -14,31 +14,55 @@ interface PrizePlaceProps {
 const types = [
     { value: 'type1', label: 'Type 1' },
     { value: 'type2', label: 'Type 2' },
-    { value: 'type3', label: 'Type 3' }
+    { value: 'type3', label: 'Type 3' },
 ]
 
-export const PrizePlace = ({winIcon, deleteIcon}: PrizePlaceProps) => {
+export const PrizePlace = ({ winIcon, deleteIcon }: PrizePlaceProps) => {
     return (
         <VStack>
+            <HStack className='prizePlace_container'>
+                <Icon
+                    Svg={winIcon}
+                    height={44}
+                    width={44}
+                    className='winIcon'
+                />
 
-        
-        <HStack className='prizePlace_container'>
-                <Icon Svg={winIcon} height={44} width={44} className='winIcon'/>
-  
                 <VStack className='winnersNum_input_container'>
-                        <Text Tag='p' className='inputTitle'>Number of winners</Text>
-                        <Input type='number' placeholder='10' className='winnersNum_input'/>
-                    </VStack>
-                    <MainInformationCombobox title='Prize type' placeholder='Select type' options={types} width={164} />
-                    <VStack className='prizeName_input_container'>
-                        <Text Tag='p' className='inputTitle'>Prize name</Text>
-                        <Input type='text' placeholder='10 000 $' className='prizeName_input'/>
-                    </VStack>
-                <Icon Svg={deleteIcon} height={48} width={24} className='deleteIcon'/>
+                    <Text Tag='p' className='inputTitle'>
+                        Number of winners
+                    </Text>
+                    <Input
+                        type='number'
+                        placeholder='10'
+                        className='winnersNum_input'
+                    />
+                </VStack>
+                <MainInformationCombobox
+                    title='Prize type'
+                    placeholder='Select type'
+                    options={types}
+                    width={164}
+                />
+                <VStack className='prizeName_input_container'>
+                    <Text Tag='p' className='inputTitle'>
+                        Prize name
+                    </Text>
+                    <Input
+                        type='text'
+                        placeholder='10 000 $'
+                        className='prizeName_input'
+                    />
+                </VStack>
+                <Icon
+                    Svg={deleteIcon}
+                    height={48}
+                    width={24}
+                    className='deleteIcon'
+                />
+            </HStack>
 
-        </HStack>
-
-                <div className='divider'/>
+            <div className='divider' />
         </VStack>
     )
 }
