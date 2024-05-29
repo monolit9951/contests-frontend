024538fb9edcc496657027757630ef9241d6be0d
Ura427 <urakost427@gmail.com>
2024-05-29 11:40:52 +0300

    Create skeleton for redux toolkit

---
+++ src/pages/contestsCreationPage/model/services/index.ts
@@ -0,0 +1,5 @@
+import { fetchContests } from './fetchContests'
+import { fetchNextContestsPage } from './fetchNextContestsPage'
+import { fetchPopularContests } from './fetchPopularContests'
+
+export { fetchContests, fetchNextContestsPage, fetchPopularContests }
