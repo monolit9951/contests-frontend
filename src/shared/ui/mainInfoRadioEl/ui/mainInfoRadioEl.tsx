22bedf4c8005fbec0b4396c658637f33d1e05e1e
Ura427 <urakost427@gmail.com>
2024-05-29 11:45:09 +0300

    use custom div with onClick property

--- src/shared/ui/mainInfoRadioEl/ui/mainInfoRadioEl.tsx
+++ src/shared/ui/mainInfoRadioEl/ui/mainInfoRadioEl.tsx
@@ -1,5 +1,5 @@
 import { Icon } from 'shared/ui/icon'
-import { VStack } from 'shared/ui/stack'
+import { Flex, VStack } from 'shared/ui/stack'
 import { Text } from 'shared/ui/text'
 
 import './mainInfoRadioEl.scss'
@@ -22,15 +22,13 @@ export const MainInfoRadioEl = ({
             ? 'mainInfoRadioEl_container active'
             : 'mainInfoRadioEl_container'
     return (
-        /* eslint-disable jsx-a11y/click-events-have-key-events */
-        /* eslint-disable jsx-a11y/no-static-element-interactions */
-        <div onClick={() => setCurrSelected?.(text)}>
+        <Flex clickFunction={() => setCurrSelected?.(text)}>
             <VStack className={classname}>
                 <Icon Svg={svg} height={32} width={32} />
                 <Text Tag='p' className='mainInfoRadioEl_container_text'>
                     {text}
                 </Text>
             </VStack>
-        </div>
+        </Flex>
     )
 }
