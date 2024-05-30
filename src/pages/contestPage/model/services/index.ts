df63b25e1d137cefb5be3c3eef52a5cf18655646
Redddeve <popko.dmitry797@gmail.com>
2024-05-30 13:56:17 +0300

    feat: add redux for contestWorks

---
+++ src/pages/contestPage/model/services/index.ts
@@ -0,0 +1,19 @@
+import {
+    fetchMediaWorks,
+    fetchNextMediaWorks,
+    fetchPopularMediaWorks,
+} from './fetchMediaWorks'
+import {
+    fetchNextTextWorks,
+    fetchPopularTextWorks,
+    fetchTextWorks,
+} from './fetchTextWorks'
+
+export {
+    fetchMediaWorks,
+    fetchNextMediaWorks,
+    fetchNextTextWorks,
+    fetchPopularMediaWorks,
+    fetchPopularTextWorks,
+    fetchTextWorks,
+}
