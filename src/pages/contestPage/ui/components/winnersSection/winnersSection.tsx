0f37f654a185489f929708768dec5b179e2c94fd
Redddeve <popko.dmitry797@gmail.com>
2024-06-20 14:26:37 +0300

    feat: add server connection to the winners table

--- src/pages/contestPage/ui/components/winnersSection/winnersSection.tsx
+++ src/pages/contestPage/ui/components/winnersSection/winnersSection.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react'
 import clsx from 'clsx'
 import { TopWinners } from 'entities/contest'
+import { Prize } from 'entities/prize'
 import { WorkCard } from 'entities/work'
+import { selectContestPrizes } from 'pages/contestPage/model/selectors'
+import { useAppSelector } from 'shared/lib/store'
 import { VStack } from 'shared/ui/stack'
 import { Text } from 'shared/ui/text'
 import { ContestWinnersTable } from 'widgets/winnersTable'
@@ -16,6 +19,11 @@ interface Props {
 const WinnersSection: FC<Props> = (props) => {
     const { data, className } = props
 
+    const prizes = useAppSelector(selectContestPrizes) as Prize[]
+
+    const otherPrizes =
+        data.length < prizes.reduce((acc, item) => acc + item.winnersAmount, 0)
+
     return (
         <section className={clsx('winners', className)}>
             <Text Tag='h2' size='title' bold className='winners__title'>
@@ -27,12 +35,19 @@ const WinnersSection: FC<Props> = (props) => {
                     <WorkCard key={work.id} data={work} prizeId={prizeId} />
                 ))}
             </ul>
-            <VStack className='winners__other'>
-                <Text Tag='h3' size='xl' bold className='winners__other-title'>
-                    Other places
-                </Text>
-                <ContestWinnersTable />
-            </VStack>
+
+            {otherPrizes && (
+                <VStack className='winners__other'>
+                    <Text
+                        Tag='h3'
+                        size='xl'
+                        bold
+                        className='winners__other-title'>
+                        Other places
+                    </Text>
+                    <ContestWinnersTable />
+                </VStack>
+            )}
         </section>
     )
 }
