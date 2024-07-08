aa7e403d829de77dff60c06eb996f4cee55c3fe6
Redddeve <popko.dmitry797@gmail.com>
2024-07-08 12:25:54 +0300

    feat: add category theme to useTheme hook

--- src/entities/theme/config/themeContext.ts
+++ src/entities/theme/config/themeContext.ts
@@ -5,11 +5,21 @@ export enum Theme {
     DARK = 'dark',
 }
 
+export enum CategoryTheme {
+    ALL = 'all',
+    FOR_FUN = 'fun',
+    FOR_WORK = 'work',
+}
+
 export interface IThemeContext {
     theme?: Theme
+    categoryTheme?: CategoryTheme
     setTheme?: (theme: Theme) => void
+    setCategoryTheme?: (categoryTheme: CategoryTheme) => void
 }
 
 export const ThemeContext = createContext<IThemeContext>({})
 
 export const LOCAL_STORAGE_THEME_KEY = 'theme'
+
+export const LOCAL_STORAGE_CATEGORY_THEME_KEY = 'categoryTheme'
--- src/entities/theme/lib/useTheme.tsx
+++ src/entities/theme/lib/useTheme.tsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react'
 import {
+    CategoryTheme,
+    LOCAL_STORAGE_CATEGORY_THEME_KEY,
     LOCAL_STORAGE_THEME_KEY,
     Theme,
     ThemeContext,
@@ -7,11 +9,14 @@ import {
 
 interface UseThemeResults {
     readonly theme?: Theme
+    readonly categoryTheme?: CategoryTheme
     readonly toggleTheme: () => void
+    readonly toggleCategoryTheme: (category: CategoryTheme) => void
 }
 
 export const useTheme = (): UseThemeResults => {
-    const { theme, setTheme } = useContext(ThemeContext)
+    const { theme, categoryTheme, setTheme, setCategoryTheme } =
+        useContext(ThemeContext)
 
     const toggleTheme = () => {
         const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK
@@ -19,5 +24,26 @@ export const useTheme = (): UseThemeResults => {
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
     }
 
-    return { theme, toggleTheme }
+    const toggleCategoryTheme = (category: CategoryTheme) => {
+        const newCategoryTheme = () => {
+            switch (category) {
+                case CategoryTheme.FOR_FUN:
+                    return CategoryTheme.FOR_FUN
+
+                case CategoryTheme.FOR_WORK:
+                    return CategoryTheme.FOR_WORK
+
+                default:
+                    return CategoryTheme.ALL
+            }
+        }
+
+        setCategoryTheme?.(newCategoryTheme())
+        localStorage.setItem(
+            LOCAL_STORAGE_CATEGORY_THEME_KEY,
+            newCategoryTheme()
+        )
+    }
+
+    return { theme, categoryTheme, toggleCategoryTheme, toggleTheme }
 }
