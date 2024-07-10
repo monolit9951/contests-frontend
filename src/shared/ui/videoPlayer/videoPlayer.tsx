56f9956c0ae2a5c254ee393793572680927d70e1
Redddeve <popko.dmitry797@gmail.com>
2024-07-10 13:35:26 +0300

    fix: fix preview image display

--- src/shared/ui/videoPlayer/videoPlayer.tsx
+++ src/shared/ui/videoPlayer/videoPlayer.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import ReactPlayer from 'react-player/lazy'
 import clsx from 'clsx'
 
@@ -13,20 +13,105 @@ interface Props {
 }
 
 const Video: FC<Props> = (props) => {
-    const { url, width = '100%', height = 612, light, className } = props
+    const { url, width = '100%', height = '100%', light, className } = props
+
+    const videoRef = useRef<HTMLVideoElement>(null)
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const [previewImage, setPreviewImage] = useState('')
+    useEffect(() => {}, [])
+
+    if (light) {
+        useEffect(() => {
+            const captureFirstFrame = () => {
+                const video = videoRef.current
+                const canvas = canvasRef.current
+                if (video && canvas) {
+                    const context = canvas.getContext('2d')
+                    if (context) {
+                        context.drawImage(
+                            video,
+                            0,
+                            0,
+                            canvas.width,
+                            canvas.height
+                        )
+                        const dataUrl = canvas.toDataURL('image/png')
+                        setPreviewImage(dataUrl)
+                    }
+                }
+            }
+
+            const handleLoadedMetadata = () => {
+                const video = videoRef.current
+                if (video) {
+                    video.currentTime = 0
+                    video.addEventListener('seeked', captureFirstFrame, {
+                        once: true,
+                    })
+                }
+            }
+
+            const video = videoRef.current
+            if (video) {
+                video.addEventListener('loadedmetadata', handleLoadedMetadata, {
+                    once: true,
+                })
+            }
+
+            return () => {
+                if (video) {
+                    video.removeEventListener(
+                        'loadedmetadata',
+                        handleLoadedMetadata
+                    )
+                }
+            }
+        }, [url])
+    }
 
     return (
-        <ReactPlayer
-            url={url}
-            width={width}
-            height={height}
-            light={light}
-            playing
-            loop
-            controls
-            volume={1}
-            className={clsx(className)}
-        />
+        <>
+            <ReactPlayer
+                url={url}
+                width={width}
+                height={height}
+                light={
+                    light &&
+                    (previewImage ? (
+                        <img
+                            src={previewImage}
+                            alt='Video preview'
+                            width={width}
+                            height={height}
+                        />
+                    ) : (
+                        <div
+                            className='video-preview-skeleton'
+                            style={{ width, height }}
+                        />
+                    ))
+                }
+                playing
+                loop
+                controls
+                volume={1}
+                className={clsx(className)}
+            />
+
+            {light && (
+                <>
+                    {/*  eslint-disable-next-line jsx-a11y/media-has-caption */}
+                    <video
+                        ref={videoRef}
+                        src={url}
+                        preload='metadata'
+                        style={{ display: 'none' }}
+                        crossOrigin='anonymous'
+                    />
+                    <canvas ref={canvasRef} style={{ display: 'none' }} />
+                </>
+            )}
+        </>
     )
 }
 
