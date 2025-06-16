64ce4aba2762f85e0c2e1976534749cb87ee8378
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-06-16 21:21:32 +0300

    header and burger design fix

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -5,8 +5,12 @@ import { useAppDispatch } from 'shared/lib/store'
 import { Logo } from 'shared/ui/logo'
 import { Searchbar } from 'shared/ui/searchbar'
 import { UserPanel } from 'widgets/userPanel'
-
+import burger from 'shared/assets/icons/burger.svg?react'
 import './header.scss'
+import { Icon } from 'shared/ui/icon'
+import clsx from 'clsx'
+import { mockNavData } from 'app/api'
+import { NavElement } from 'shared/ui/navElement'
 
 export const Header = () => {
     const [inputData, setInputData] = useState('')
@@ -23,9 +27,28 @@ export const Header = () => {
         setInputData('')
     }
 
+    // открытие навигационного меню
+    const [sidebar, setSideBar] = useState<boolean>(false)
+
+    const toggleSideBar = () => {
+        setSideBar(!sidebar)
+    }
+
     return (
         <header className='header'>
-            <Logo />
+            <div className="header_logoGroup">
+                <div className='burgerMenu'>
+                    <Icon
+                        Svg={burger}
+                        height={36}
+                        width={36}
+                        clickable
+                        onClick={toggleSideBar}
+                    />
+                </div>
+
+                <Logo />
+            </div>
             <Searchbar
                 searchData={inputData}
                 onChange={setInputData}
@@ -33,6 +56,14 @@ export const Header = () => {
                 placeholder='Search by any parameters....'
             />
             <UserPanel />
+
+            <div className={clsx('header_sideNavBar', { open: sidebar })}>
+                <nav>
+                    {mockNavData.map((navItem, index) => (
+                        <NavElement key={index} {...navItem} />
+                    ))}
+                </nav>
+            </div>
         </header>
     )
 }
