890271704266d9bfbc3b926472219638891f3da9
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-06-18 17:00:30 +0300

    videoPlayer

--- src/pages/battlesPage/ui/battlesPage.tsx
+++ src/pages/battlesPage/ui/battlesPage.tsx
@@ -1,7 +1,14 @@
 import "./battlesPage.scss"
+import Plyr from "plyr-react"
+import verticalVideo from '../../../shared/assets/testVideos/testVideo.mp4'
+import "plyr-react/plyr.css";
+import { Video } from "shared/ui/videoPlayer";
 
 export const BattlesPage = () => {
     return(
-        <h1>BattlesPage</h1>
+        <div className="battlesPage_container">
+            {/* <Plyr source={{type: "video", sources: [{src: verticalVideo, type: "video/mp4"}]}}/> */}
+            <Video url={verticalVideo}/>
+        </div>
     )
 }
--- src/shared/ui/videoPlayer/videoPlayer.tsx
+++ src/shared/ui/videoPlayer/videoPlayer.tsx
@@ -2,8 +2,10 @@ import { FC, useEffect, useRef, useState } from 'react'
 import ReactPlayer from 'react-player/lazy'
 import clsx from 'clsx'
 import Plyr from 'plyr-react'
+import "plyr-react/plyr.css";
 import './videoPlayer.scss'
 import sampleVideo from '../../../shared/assets/testVideos/testVideo.mp4'
+import { HStack } from '../stack';
 
 interface Props {
     url: string
@@ -16,71 +18,6 @@ interface Props {
 const Video: FC<Props> = (props) => {
     const { url, width = '100%', height = '100%', light, className } = props
 
-    const videoRef = useRef<HTMLVideoElement>(null)
-    const canvasRef = useRef<HTMLCanvasElement>(null)
-    const previewDivRef = useRef<HTMLDivElement>(null)
-    const [previewImage, setPreviewImage] = useState('')
-
-    if (light) {
-        useEffect(() => {
-            const captureFirstFrame = () => {
-                const video = videoRef.current
-                const canvas = canvasRef.current
-
-                if (video && canvas) {
-                    const context = canvas.getContext('2d')
-                    if (context) {
-                        context.drawImage(
-                            video,
-                            0,
-                            0,
-                            canvas.width,
-                            canvas.height
-                        )
-                        const dataUrl = canvas.toDataURL('image/png')
-                        setPreviewImage(dataUrl)
-                    }
-                }
-            }
-
-            const handleLoadedMetadata = () => {
-                const video = videoRef.current
-                if (video) {
-                    video.currentTime = 0
-                    video.addEventListener('seeked', captureFirstFrame)
-                }
-            }
-
-            const video = videoRef.current
-            if (video) {
-                video.addEventListener('loadedmetadata', handleLoadedMetadata)
-            }
-
-            return () => {
-                if (video) {
-                    video.removeEventListener(
-                        'loadedmetadata',
-                        handleLoadedMetadata
-                    )
-                }
-            }
-        }, [url])
-    }
-
-    useEffect(() => {
-        if (previewImage) {
-            const previewDiv = previewDivRef.current
-
-            if (previewDiv) {
-                const backgroundImage = window
-                    .getComputedStyle(previewDiv)
-                    .getPropertyValue('background-image')
-                if (!backgroundImage || backgroundImage === 'none') {
-                    previewDiv.style.backgroundImage = `url(${previewImage})`
-                }
-            }
-        }
-    }, [previewImage])
 
     // настройки проигрывателя Plyr
     const plyrOptions = {
@@ -105,42 +42,9 @@ const Video: FC<Props> = (props) => {
     }
 
     return (
-        <>
-            <ReactPlayer
-                url={sampleVideo}
-                width={width}
-                height={height}
-                light={
-                    light && (
-                        <div
-                            ref={previewDivRef}
-                            className='react-player__preview'
-                        />
-                    )
-                }
-                playing
-                loop
-                controls
-                volume={1}
-                className={clsx(className)}
-            />
-
-            {/* <Plyr className='plur_video' options={plyrOptions} source={{type: 'video', sources: [{src: sampleVideo, type: 'video/mp4'}]}}/> */}
-
-            {light && (
-                <>
-                    {/*  eslint-disable-next-line jsx-a11y/media-has-caption */}
-                    <video
-                        ref={videoRef}
-                        src={sampleVideo}
-                        preload='metadata'
-                        style={{ display: 'none' }}
-                        crossOrigin='anonymous'
-                    />
-                    <canvas ref={canvasRef} style={{ display: 'none' }} />
-                </>
-            )}
-        </>
+        <div className='video_container'>
+            <Plyr source={{type: "video", sources: [{src: sampleVideo, type: "video/mp4"}]}}/>
+        </div>
     )
 }
 
