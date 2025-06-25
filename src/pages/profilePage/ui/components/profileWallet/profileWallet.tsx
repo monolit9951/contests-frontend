02e3d3e8198e39a8b57f7e51d8ad94b5c85f48dd
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-06-25 21:25:17 +0300

    profilePage left side

--- src/pages/profilePage/ui/components/profileWallet/profileWallet.tsx
+++ src/pages/profilePage/ui/components/profileWallet/profileWallet.tsx
@@ -1,13 +1,58 @@
 import { FC } from "react";
 import './profileWallet.scss'
+import profileWallet from 'shared/assets/icons/profileWallet.svg'
+
 
 const ProfileWallet: FC = () =>{
+
+    // ВОЗМОЖНО СДЕЛАТЬ ИСТОРИЮ БАЛАНСА В ВИДЕ ОТЛЕЛЬНЫХ КОМПОНЕНТОВ, ЕСЛИ ЛОГИКА БУДЕТ СЛОЖНЕЕ
+
     return(
         <div className="profileWallet">
             <div className="profileWallet_header">
                 <div className="profileWallet_header_heading">Wallet</div>
                 
-                <img src="" alt="wallet" />
+                <img src={profileWallet} alt="wallet" />
+            </div>
+
+            <div className="profileWallet_currentBalanceGroup">
+                <div className="profileWallet_currentBalanceGroup_balance">$ 12,450.75</div>
+                <div className="profileWallet_currentBalanceGroup_desc">Current Balance</div>
+            </div>
+
+            <div className="profileWallet_balance_manipulation">
+                <button>Deposit</button>
+                <button>Withdraw</button>
+            </div>
+
+            <div className="profileWallet_balance_history">
+                
+                <div className="profileWallet_balance_action">
+                    <div className="profileWallet_balance_action_left">
+                        <div className="profileWallet_balance_action_type">+</div>
+
+                        <div className="profileWallet_balance_action_info">
+                            <div className="profileWallet_balance_action_info_name">Contest Prize</div>
+                            <div className="profileWallet_balance_action_info_desc">2024-01-15</div>
+                        </div>
+                    </div>
+
+                    <div className="profileWallet_balance_action_right">+$500</div>
+                </div>
+
+                <div className="profileWallet_balance_action">
+                    <div className="profileWallet_balance_action_left">
+                        <div className="profileWallet_balance_action_type">+</div>
+
+                        <div className="profileWallet_balance_action_info">
+                            <div className="profileWallet_balance_action_info_name">Contest Prize</div>
+                            <div className="profileWallet_balance_action_info_desc">2024-01-15</div>
+                        </div>
+                    </div>
+
+                    <div className="profileWallet_balance_action_right">+$500</div>
+                </div>
+
             </div>
         </div>
     )
