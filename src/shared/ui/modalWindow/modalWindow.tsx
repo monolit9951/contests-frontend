d134ac4df758cb5393f276239bde8b2722411712
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-07-16 16:28:36 +0300

    correct parent modalWindow

--- src/shared/ui/modalWindow/modalWindow.tsx
+++ src/shared/ui/modalWindow/modalWindow.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, useEffect, useState } from 'react'
+import React, { Children, FC, ReactNode, useEffect, useState } from 'react'
 import clsx from 'clsx'
 import { useTheme } from 'entities/theme'
 import xIcon from 'shared/assets/icons/Secondary_btn.svg?react'
@@ -35,22 +35,7 @@ export const ModalWindow: FC<UploadModalProps> = ({
         { 'modal-open': rest.isOpen },
         rest.className
     )
-
-
-    // статус анимации
-    const [hideAnimation, setHideAnimation] = useState<boolean>(false)
-
-    // для анимированного анмаунта 
-    const handleClose = () =>{
-        setHideAnimation(true)
-        if(rest.onClose){
-            setTimeout(() => {
-                setHideAnimation(false)
-                rest.onClose()
-            }, 300);
-        }
-    }
-
+    
     // БЛОКИРОВКА СКОЛЛА
     useEffect(() => {
         const body = document.body as HTMLElement
@@ -67,33 +52,20 @@ export const ModalWindow: FC<UploadModalProps> = ({
         return null
     }
 
-    const overlayClass = clsx('modal-overlay', {hide: hideAnimation}, rest.overlayClassName)
-    const modalContentClass = clsx('modal-content', {hide: hideAnimation}, rest.modalContentClass, theme)
+
+    const handleClose = () =>{
+        if(rest.onClose){
+            rest.onClose()
+        }
+    }
 
     return (
-        <VStack className={modalClass}>
-            <div className='modal-wrapper' style={{ width, height, maxWidth, maxHeight }}>
-                <Flex className={overlayClass} clickFunction={handleClose} />
-                <HStack className={modalContentClass}>
-                    
-                    <VStack>
-                        {typeof rest.children === 'object' && React.isValidElement(rest.children)
-                            ? React.cloneElement(rest.children, { onClose: handleClose })
-                            : rest.children}
-                    </VStack>
+        <div className="modalWindow">
+            <div className="modalWindow_background" onClick={handleClose}/>
 
-                </HStack>
-                {rest.isOuterClose && (
-                    <Icon
-                        Svg={xIcon}
-                        width={36}
-                        height={36}
-                        clickable
-                        onClick={handleClose}
-                        btnClassName='modal-x'
-                    />
-                )}
+            <div className="modalWindow_overlay">
+                {rest.children}
             </div>
-        </VStack>
+        </div>
     )
 }
