d196ba356741f7f924bea5a29115ded5b3407019
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-08-05 15:26:54 +0300

    work modal page logic

---
+++ src/pages/modalWorkPage/ui/modalWorkPage.tsx
@@ -0,0 +1,30 @@
+import { Work } from "entities/work";
+import { FC } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import useAxios from "shared/lib/hooks/useAxios";
+import { ModalWindow } from "shared/ui/modalWindow";
+import { WorkPreview } from "widgets/worksSection/ui/workPreview/workPreview";
+
+const ModalWorkPage: FC = () => {
+    
+    const navigate = useNavigate()
+    const {contestId, workId} = useParams()
+
+    const handlePageClose = () =>{
+        if(contestId){
+            navigate(`/contests/${contestId}`)
+        } else {
+            navigate(`/`)
+        }
+    }
+
+    const {data, isLoading} = useAxios<Work>(`works/${workId}`)
+
+    return(
+        <ModalWindow isOpen onClose={handlePageClose} >
+            {!isLoading && data && <WorkPreview onClose={handlePageClose} work={data}/>}
+        </ModalWindow>
+    )
+}
+
+export default ModalWorkPage
