cd1b631f69d51c8fb04bf0b2560a0bc409ecbe1e
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-08-15 19:08:43 +0300

    controlledSelector prev styles

--- src/shared/ui/controlledSelector/ui/controlledSelector.tsx
+++ src/shared/ui/controlledSelector/ui/controlledSelector.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import tick from 'shared/assets/icons/fullTick.svg'
 
 interface Option {
   value: string;
@@ -9,24 +10,34 @@ interface SelectProps {
   options: Option[];
   value: string | null;
   onChange: (value: string) => void;
+  error?: boolean
 }
 
-const ControlledSelector: FC<SelectProps> = ({ options, value, onChange }) => {
-  return (
-    <div className="border rounded-md w-48">
-      {options.map((option) => (
-        <div
-          key={option.value}
-          className={`px-3 py-2 cursor-pointer hover:bg-gray-100 ${
-            option.value === value ? "bg-gray-200 font-semibold" : ""
-          }`}
-          onClick={() => onChange(option.value)}
-        >
-          {option.label}
+const ControlledSelector: FC<SelectProps> = ({ options, value, onChange, error = false}) => {
+
+    const maxWidth = 300
+    
+    const [selectorOpen, setSelectorOpen] = useState<boolean>(false)
+
+    const handleSelectorToggle =() => {
+        setSelectorOpen(!selectorOpen)
+    }
+
+
+    return (
+        <div className="customSelector" style={{maxWidth}}>
+            <button className={error? "customSelector_header error" : "customSelector_header"} type="button" onClick={handleSelectorToggle} >
+                <span className={selectorOpen? "open" : ""}>{value}</span>
+                <img className={selectorOpen? "open" : ""} src={tick} alt="tick" />
+            </button>
+
+            {selectorOpen && <div className="customSelector_options">
+                {options.map((item: Option, index: number) => (
+                    <button type="button" key={index} className="customSelector_options_option" onClick={() => onChange('da')}>{item.label}</button>
+                ))}
+            </div>}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default ControlledSelector;
