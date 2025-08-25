85e5f61a6c6f9d0f056379629a920733d1d7b917
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-08-25 14:22:51 +0300

    new feed page

--- src/entities/work/ui/workComponent.tsx
+++ src/entities/work/ui/workComponent.tsx
@@ -14,13 +14,13 @@ import { UserIcon } from 'shared/ui/userIcon'
 import { Video } from 'shared/ui/videoPlayer'
 
 import './workComponent.scss'
+import VideoPlayer from 'shared/ui/videoPlayer/videoPlayer'
 
 interface WorkProps {
     work: Work
-    openModal: (work: Work) => void
 }
 
-const WorkComponent: React.FC<WorkProps> = ({ work, openModal }) => {
+const WorkComponent: React.FC<WorkProps> = ({ work }) => {
     const [isReadMore, setIsReadMore] = useState(false)
 
     const {
@@ -41,87 +41,20 @@ const WorkComponent: React.FC<WorkProps> = ({ work, openModal }) => {
         setIsReadMore(!isReadMore)
     }
 
-    const onCommentsClick = () => {
-        openModal(work)
-    }
+    console.log(work)
 
     return (
-        <div>
-            <Button
-                variant='div'
-                className='work-component'
-                onClick={() => openModal(work)}>
-                <HStack className='align__center justify__start'>
-                    <UserIcon
-                        src={user.profileImage}
-                        userName={user.name}
-                        alt={`Creator's profile picture`}
-                        size={40}
-                    />
-                    {user.verificationStatus && <Verified />}
-                    <Text Tag='span' className='work-component__timeago'>
-                        {timeAgo}
-                    </Text>
-                </HStack>
+        <div className="workComponent">
 
-                <Text Tag='p' className='work-component__text'>
-                    {(description.length < 250 && description) || (
-                        <>
-                            {!isReadMore
-                                ? description.slice(0, 250)
-                                : `${description} `}
-                            <button
-                                type='button'
-                                className='read-more-btn'
-                                onClick={toggleReadMore}>
-                                <Text Tag='span'>
-                                    {!isReadMore ? '... more' : 'show less'}
-                                </Text>
-                            </button>
-                        </>
-                    )}
-                </Text>
-
-                <HStack className={clsx('tag-contest align__center')}>
-                    <div className='icon-box'>
-                        <Icon Svg={prize} className='icon-work__prev' />
-                    </div>
-                    <Text Tag='p' size='xs' bold>
-                        Acting Talent Search
-                    </Text>
-                    <Text Tag='span' size='xs'>
-                        &#8226;
-                    </Text>
-                    <Text Tag='span' size='xs'>
-                        Active
-                    </Text>
-                </HStack>
-
-                {media?.[0]?.mediaLink && media?.[0]?.typeMedia === "IMAGE" && (
-                    <Image
-                        src={media[0].mediaLink}
-                        alt={`User's image`}
-                        width={420}
-                        height={720}
-                        className='work-component__image'
-                    />
-                )}
-                {media?.[0]?.mediaLink && media?.[0]?.typeMedia === 'VIDEO' && (
-                    <Video
-                        url={media[0].mediaLink}
-                        // width={420}
-                        // height={720}
-                        light
-                        // className='work-component__video'
-                    />
-                )}
+            <Button className="workComponent_container" variant='primary'>
+                {work.media && work.media[0].typeMedia === 'IMAGE' && <img src={work.media[0].mediaLink} alt='mediaWork' />}
+                {work.media && work.media[0].typeMedia === 'VIDEO' && <Video url={work.media[0].mediaLink} light/>}
             </Button>
 
             <MediaFeedback
                 id={id}
                 likes={likeAmount}
                 comments={commentAmount}
-                onCommentsClick={onCommentsClick}
                 className='workComponent_mediafeedback'
                 liked={userLike}
             />
--- src/widgets/worksSection/ui/worksSection.tsx
+++ src/widgets/worksSection/ui/worksSection.tsx
@@ -13,114 +13,87 @@ import {
 } from 'pages/feedPage'
 import { useAppDispatch, useAppSelector } from 'shared/lib/store'
 import { Button } from 'shared/ui/button'
-import { ModalWindow } from 'shared/ui/modalWindow'
 import Spinner from 'shared/ui/spinner'
-import { VStack } from 'shared/ui/stack'
 import { Text } from 'shared/ui/text'
-import { WorkPreview } from 'widgets/worksSection/ui/workPreview/workPreview'
 
 import './worksSection.scss'
+import useAxios from 'shared/lib/hooks/useAxios'
 
 const WorksSection: React.FC = () => {
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
-    const [selectedWork, setSelectedWork] = useState<Work | null>(null)
-    const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth)
+    // const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth)
 
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
 
-    const dispatch = useAppDispatch()
+    // const dispatch = useAppDispatch()
 
-    const works = useAppSelector(selectWorks)
-    const loading = useAppSelector(selectLoading)
-    const error = useAppSelector(selectError)
-    const page = useAppSelector(selectPage)
-    const hasMore = useAppSelector(selectHasMore)
+    // const works = useAppSelector(selectWorks)
+    // const loading = useAppSelector(selectLoading)
+    // const error = useAppSelector(selectError)
+    // const page = useAppSelector(selectPage)
+    // const hasMore = useAppSelector(selectHasMore)
 
-    const observer = useRef<IntersectionObserver | null>(null)
+    // const observer = useRef<IntersectionObserver | null>(null)
 
-    const lastWorkElementRef = useCallback(
-        (node: any) => {
-            if (loading) return
-            if (observer.current) observer.current.disconnect()
-            observer.current = new IntersectionObserver((entries) => {
-                if (entries[0].isIntersecting && hasMore) {
-                    dispatch(incrementPage())
-                }
-            })
-            if (node) observer.current.observe(node)
-        },
-        [loading, hasMore]
+    // const lastWorkElementRef = useCallback(
+    //     (node: any) => {
+    //         if (loading) return
+    //         if (observer.current) observer.current.disconnect()
+    //         observer.current = new IntersectionObserver((entries) => {
+    //             if (entries[0].isIntersecting && hasMore) {
+    //                 dispatch(incrementPage())
+    //             }
+    //         })
+    //         if (node) observer.current.observe(node)
+    //     },
+    //     [loading, hasMore]
+    // )
+
+    // useEffect(() => {
+    //     const handleResize = () => {
+    //         setWindowWidth(window.innerWidth)
+    //     }
+
+    //     window.addEventListener('resize', handleResize)
+
+    //     return () => window.removeEventListener('resize', handleResize)
+    // }, [windowWidth])
+
+    // useEffect(() => {
+    //     dispatch(fetchWorks(page))
+    // }, [dispatch, page])
+
+
+    // if (loading && !works.length) {
+    //     return <Spinner center />
+    // }
+
+    // if (error) {
+    //     return (
+    //         <div className='works-section__error-message'>
+    //             <Text Tag='p' bold size='xl'>
+    //                 Error: {error}
+    //             </Text>
+    //             <Button variant='secondary' onClick={() => navigate(-1)}>
+    //                 Go back
+    //             </Button>
+    //         </div>
+    //     )
+    // }
+
+
+    const { data, isLoading } = useAxios<Work>(
+        `works/68a8957b2a7ba106f3dcba7c`
     )
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth)
-        }
-
-        window.addEventListener('resize', handleResize)
-
-        return () => window.removeEventListener('resize', handleResize)
-    }, [windowWidth])
-
-    useEffect(() => {
-        dispatch(fetchWorks(page))
-    }, [dispatch, page])
-
-    const openModal = (work: Work) => {
-        setSelectedWork(work)
-        setIsModalOpen(true)
-    }
-
-    if (loading && !works.length) {
-        return <Spinner center />
-    }
-
-    if (error) {
-        return (
-            <div className='works-section__error-message'>
-                <Text Tag='p' bold size='xl'>
-                    Error: {error}
-                </Text>
-                <Button variant='secondary' onClick={() => navigate(-1)}>
-                    Go back
-                </Button>
-            </div>
-        )
-    }
-
-    const getModalMaxWidth = (work: Work | null): string => {
-        if (work?.typeWork === 'TEXT') {
-            return '520px'
-        }
-        return '100%'
-    }
 
     return (
-        <VStack className='works-section'>
-            {works.map((work: Work, index: number) => (
-                <div
-                    key={index}
-                    ref={works.length === index + 1 ? lastWorkElementRef : null}
-                    className='works-section__content'>
-                    <WorkComponent work={work} openModal={openModal} />
-                </div>
-            ))}
-
-            {isModalOpen && (
-                <ModalWindow
-                    isOpen={isModalOpen}
-                    onClose={() => setIsModalOpen(false)}
-                    isOuterClose
-                    maxWidth={getModalMaxWidth(selectedWork)}
-                    height={windowWidth > 1024 ? '83%' : '88%'}
-                    maxHeight={windowWidth >= 1024 ? '900px' : ''}
-                    modalContentClass='work-preview-modal'>
-                    {selectedWork && <WorkPreview work={selectedWork} />}
-                </ModalWindow>
-            )}
-
-            {loading && <Spinner />}
-        </VStack>
+        <div className="worksSection">
+            <div className="worksSection_container">
+                <ul className='worksSection_list'>
+                    {!isLoading && data && <WorkComponent work={data}/>}
+                </ul>
+            </div>
+        </div>
     )
 }
 
