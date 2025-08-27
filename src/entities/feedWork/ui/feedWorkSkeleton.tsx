8764a4e2ec9c52c9faa335855ebd2aebb4e0b560
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-08-27 18:36:56 +0300

    feedWorkSkeleton adaptive fixes

--- src/entities/feedWork/ui/feedWorkSkeleton.tsx
+++ src/entities/feedWork/ui/feedWorkSkeleton.tsx
@@ -1,22 +1,42 @@
-import Skeleton from "react-loading-skeleton"
+import Skeleton from "react-loading-skeleton";
 
+import "react-loading-skeleton/dist/skeleton.css";
 import './feedWorkSkeleton.scss'
 
-const FeedWorkSkeleton = () =>{
-    return(
-        <div className="feedWorkSkeleton">
-            <div className="feedWorkSkeleton_profile">
-                <Skeleton width={50} height={50} borderRadius={25}/>
+const FeedWorkSkeleton = () => {
+  return (
+    <div className="feedWorkSkeleton">
+      <div className="feedWorkSkeleton_profile">
+        <Skeleton width={50} height={50} borderRadius={25} />
 
-                <div className="feedWorkSkeleton_profile_info">
-                    <Skeleton width={200} height={23} />
-                    <Skeleton width={100} height={23} />
-                </div>
-            </div>
-            
-            <Skeleton height={612} borderRadius={24} className="feedWorkSkeleton_media"/>
+        <div className="feedWorkSkeleton_profile_info">
+          <Skeleton width={200} height={23} />
+          <Skeleton width={100} height={23} />
         </div>
-    )
-}
+      </div>
 
-export default FeedWorkSkeleton
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          maxWidth: 10000,
+          minWidth: 250,
+          paddingTop: `${(612 / 455) * 100}%`, // соотношение 455:612
+        }}
+      >
+        <Skeleton
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            borderRadius: 24,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FeedWorkSkeleton;
