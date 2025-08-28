c86c38565295a58b6d6f0b8f5da93dd62b777116
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-08-28 16:00:41 +0300

    tanstack on feedPage attempt

--- src/widgets/worksSection/ui/worksSection.tsx
+++ src/widgets/worksSection/ui/worksSection.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 // import Spinner from 'shared/ui/spinner'
-import {useInfiniteQuery} from '@tanstack/react-query'
+import {useInfiniteQuery, useQueryClient} from '@tanstack/react-query'
 import { Work } from 'entities/work'
 import WorkComponent from 'entities/work/ui/workComponent'
 import { fetchFeedWorks } from 'pages/feedPage/model/services/fetchWorks'
@@ -45,6 +45,10 @@ const WorksSection: React.FC = () => {
             lastPage.content.length === 3 ? allPages.length + 1 : undefined,
     });
 
+
+
+    console.log(works)
+
     const handleObserver = useCallback(
         (entries: IntersectionObserverEntry[]) => {
         const target = entries[0];
@@ -55,6 +59,7 @@ const WorksSection: React.FC = () => {
         },
         [hasNextPage, isFetchingNextPage, fetchNextPage]
     );
+
     useEffect(() => {
         const option = { root: null, rootMargin: '20px', threshold: 0 };
         const observer = new IntersectionObserver(handleObserver, option);
@@ -65,17 +70,38 @@ const WorksSection: React.FC = () => {
             };
     }, [handleObserver]);
 
+    const queryClient = useQueryClient();
+
+    const updateDescription = (workId: string, newDescription: string) => {
+        queryClient.setQueryData(['feedWorks'], (oldData: any) => {
+            if (!oldData) return oldData;
+
+            return {
+            ...oldData,
+            pages: oldData.pages.map((page: any) => ({
+                ...page,
+                content: page.content.map((work: any) =>
+                work.id === workId ? { ...work, description: newDescription } : work
+                ),
+            })),
+            pageParams: oldData.pageParams,
+            };
+        });
+    };
+
 
     return (
         <div className='worksSection'>
+            
             <div className='worksSection_container'>
+                <button type='button' onClick={() => updateDescription('68aefd0d4437153ad8d1c5d9', 'Новый текст описания')}>CHANGE</button>
                 <div>
                     {works?.pages.map((page, pageIndex) => (
-                    <ul key={pageIndex}>
-                        {page.content.map((data: Work) => (
-                        <WorkComponent work={data} key={data.id} />
-                        ))}
-                    </ul>
+                        <ul key={pageIndex}>
+                            {page.content.map((data: Work) => (
+                                <WorkComponent work={data} key={data.id} />
+                            ))}
+                        </ul>
                     ))}
                 </div>
 
@@ -83,6 +109,7 @@ const WorksSection: React.FC = () => {
                 {isFetchingNextPage && <p>Загрузка...</p>}
                 {!hasNextPage && <p>Больше новостей нет</p>}
             </div>
+            
 
             {workPreviewId && (
                 <ModalWindow isOpen onClose={handleCloseModal}>
