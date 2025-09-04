2b8ca2f08f730ef6c71ac6bfa4745112324186de
ivankalituk <63204841+ivankalituk@users.noreply.github.com>
2025-09-04 14:25:16 +0300

    contests page design fixes

--- src/widgets/heroSection/ui/heroSection.tsx
+++ src/widgets/heroSection/ui/heroSection.tsx
@@ -1,6 +1,6 @@
 import { CategoryFilter } from "shared/ui/categoryFilter"
 import { Text } from "shared/ui/text";
-
+import background from 'shared/assets/img/bg@1x.jpg'
 import "./heroSection.scss"
 
 export const HeroSection = () => {
@@ -8,6 +8,7 @@ export const HeroSection = () => {
         <div className="heroSection_container">
             <Text Tag="h2">Find a contest for yourself</Text>
             <Text Tag="p">Bring your best jokes, wittiest puns, and most infectious laughter,<br/> because in this competition, the funniest wins!</Text>
+            <img src={background} alt="background" />
             <CategoryFilter/>
         </div>
     )
